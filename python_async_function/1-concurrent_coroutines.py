78b0a29bc588cff444656ad78d14cd921e06c95c
JeremiasInCode <jeremiaserba7894@gmail.com>
2024-01-04 13:16:06 -0300

    Task 1

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" asynchronous coroutine """
+import asyncio
+import random
+from typing import List
+
+
+wait_random = __import__("0-basic_async_syntax").wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """return the list of all the delays in ascending order"""
+    completed_task_lst = []
+
+    for _ in range(n):
+        completed_task_lst.append(await wait_random(max_delay))
+
+    return sorted(completed_task_lst)
