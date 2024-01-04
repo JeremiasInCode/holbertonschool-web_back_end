96adba634743003acdd99e166d2c4850cf0377f1
JeremiasInCode <jeremiaserba7894@gmail.com>
2024-01-04 13:46:29 -0300

    Task 0

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""coroutine"""
+import asyncio
+import time
+from typing import List
+
+
+async_comprehension = __import__("1-async_comprehension").async_comprehension
+
+
+async def measure_runtime() -> float:
+    """ coroutine that will execute async_comprehension """
+    start_time = time.time()
+    await asyncio.gather(async_comprehension())
+    end_time = time.time()
+
+    return end_time - start_time
