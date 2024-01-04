b70dcaebe276db1d84315e7436a9cf350d07176f
JereDev19 <jeremiaserba7894@gmail.com>
2024-01-04 14:43:58 -0300

    Fixes task 0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -6,7 +6,7 @@ import typing
 
 
 async def async_generator() -> typing.Generator[float, None, None]:
-    """asynchronous coroutine that generates random float numbers"""
+    """asynchronous coroutine"""
     for _ in range(0, 10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
