9530cae6751c8c49aac6b5fc273526a04fdb02cb
JereDev19 <jeremiaserba7894@gmail.com>
2024-01-08 15:15:11 -0300

    Task 2 - fix output

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -38,7 +38,7 @@ class Server:
     def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
         """ get_hyper function """
         metadata = self.get_page(page, page_size)
-        total_pages = math.ceil(len(self.dataset())) / page_size
+        total_pages = math.ceil(len(self.dataset()) / page_size)
         return {
             "page_size": len(metadata),
             'page': page,
