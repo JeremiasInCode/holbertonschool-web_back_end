5f75bcbf3240d5d373b97a7429655ccfdfb19a94
Jeremias Erba <jeremiaserba7894@gmail.com>
2024-01-16 10:08:14 -0300

    Fix eslint and fix end line

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -2,7 +2,7 @@ import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
-    super(sqft)
+    super(sqft);
     this._floors = floors;
   }
 
@@ -11,6 +11,6 @@ export default class SkyHighBuilding extends Building {
   }
 
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this.floors} floors`
+    return `Evacuate slowly the ${this.floors} floors`;
   }
 }
