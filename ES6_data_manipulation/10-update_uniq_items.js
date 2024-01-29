82a9c8d0714ef1ac1ce92123c595214c089ef78c
Jeremias Erba <jeremiaserba7894@gmail.com>
2024-01-29 19:00:59 -0300

    Failed declaration of updateUniqueItems function, and fix error by eslint

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,11 +1,10 @@
-export function updateUniqueItems(aMap) {
+export default function updateUniqueItems(aMap) {
   if (!(aMap instanceof Map)) {
     throw new Error('Cannot process');
   }
-
   aMap.forEach((value, key) => {
     if (value === 1) {
       aMap.set(key, 100);
     }
-  })
+  });
 }
