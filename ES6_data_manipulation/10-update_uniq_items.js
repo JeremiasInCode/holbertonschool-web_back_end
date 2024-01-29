abb75f84dc305e56cb9b9621794d7124f51d4c1b
Jeremias Erba <jeremiaserba7894@gmail.com>
2024-01-29 18:55:36 -0300

    Failed in verification with instaceof

---
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,11 @@
+export function updateUniqueItems(aMap) {
+  if (!(aMap instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+
+  aMap.forEach((value, key) => {
+    if (value === 1) {
+      aMap.set(key, 100);
+    }
+  })
+}
